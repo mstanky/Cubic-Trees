dfb405a9e43748e87f6e328dd4d8be011db624f6
mstanky <mstanky96@gmail.com>
2018-02-28 08:58:38 -0500

    Added unit test for rooted matrix.

--- src/test/scala/ProjectTest.scala
+++ src/test/scala/ProjectTest.scala
@@ -5,7 +5,7 @@ import TreeGenerator._
 
 class ProjectTest extends FunSpec with Matchers{
     describe("The Root object"){
-        describe("can created a rooted adjacency matrix from a regular one that"){
+        describe("can create a rooted adjacency matrix from a regular one that"){
             val rootedAdjMat = makeRootedMatrix(treeOne)
             it("has one degree-3 node."){
                 assert( rootedAdjMat.filter( _.count(_ == 1) == 3 ).size == 1 )
