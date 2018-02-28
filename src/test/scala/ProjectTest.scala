a9135180643d198f8514569139763a764441a016
mstanky <mstanky96@gmail.com>
2018-02-28 08:27:33 -0500

    Added unit test for rooted matrix.

--- src/test/scala/ProjectTest.scala
+++ src/test/scala/ProjectTest.scala
@@ -3,26 +3,20 @@ import org.scalatest.Matchers
 
 class ProjectTest extends FunSpec with Matchers{
     describe("The TreeReader object"){
-        it("can tell whether there is another tree to be tested."){
-            ???
-        }
+      it("can tell whether there is another tree to be tested."){
+          ???
+      }
     }
     describe("The DatWriter boject"){
-        it("can convert an adjacency matrix to an adjacency list."){
-            val treeOne = Array(Array(0,1,0,0,0,0,0,0,0,0,0,0),
-                          Array(1,0,1,0,0,0,1,0,0,0,0,0),
-                          Array(0,1,0,1,0,0,0,1,0,0,0,0),
-                          Array(0,0,1,0,1,0,0,0,1,0,0,0),
-                          Array(0,0,0,1,0,1,0,0,0,1,0,0),
-                          Array(0,0,0,0,1,0,0,0,0,0,0,0),
-                          Array(0,1,0,0,0,0,0,0,0,0,0,0),
-                          Array(0,0,1,0,0,0,0,0,0,0,1,1),
-                          Array(0,0,0,1,0,0,0,0,0,0,0,0),
-                          Array(0,0,0,0,1,0,0,0,0,0,0,0),
-                          Array(0,0,0,0,0,0,0,1,0,0,0,0),
-                          Array(0,0,0,0,0,0,0,1,0,0,0,0))
-            val out = 
-            assert(DatWriter.toAdjList(treeOne) == out)
+          ???
+    }
+    describe("The Root object"){
+        import Root._
+        describe("can created a rooted adjacency matrix from a regular one that"){
+            val rootedAdjMat = createdRootedMatrix(treeOne)
+            it("has one degree-3 node."){
+                assert( oneRooted.filter( _.count(_ == 1) == 3 ).size == 1 )
+            }
         }
     }
 }
