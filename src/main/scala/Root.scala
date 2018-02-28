4b31e94753e581666794d6eb5adc30284c2caed6
mstanky <mstanky96@gmail.com>
2018-02-28 08:09:03 -0500

    Diagnosing rootCanonicalName

--- src/main/scala/Root.scala
+++ src/main/scala/Root.scala
@@ -50,9 +50,10 @@ object Root {
 			if(rootedMat(node).filter(_==1).isEmpty) "10"
 			else{
 				var childrenNames = List.empty[String]
-				for(child <- findNeighbors(rootedMat,node))
+				for(child <- findNeighbors(rootedMat,node)){
 					println(child)
 					childrenNames = assignCanonicalNames(child) :: childrenNames
+				}
 				//val children = findNeighbors(rootedMat,node).map(child => assignCanonicalNames(child)).sortWith(_.length < _.length)
 				"1 " + childrenNames.mkString(" ") + " 0"
 			}
